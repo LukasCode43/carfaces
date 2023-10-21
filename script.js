32d521d4bee244409e2846cde5f2c0c767c0991b
LukasCode43 <ober.lukas46@gmail.com>
2023-10-21 14:40:52 +0200

    first js

---
+++ script.js
@@ -0,0 +1,28 @@
+let picsUrl = ["pictures/red honda.jpg", "pictures/blue car.avif", "pictures/green bmw.jpg"];
+let imgIndex = 1; 
+
+
+
+function change() {
+    // Wählt das Bild-Element aus
+    let imgElement = document.querySelector(".picture img");
+
+    // Ändert das src-Attribut des Bildes
+    imgElement.src = picsUrl[imgIndex];
+    imgIndex = (imgIndex + 1) % picsUrl.length;
+    
+    if(imgIndex == 0) {
+        imgElement.remove();
+        
+        const newP = document.createElement("p");
+        const t = document.createTextNode("You have gone through all the pictures. There will soon be new ones");
+        newP.appendChild(t);
+
+        const pictureDiv = document.querySelector(".picture");
+        document.body.append(newP);
+        const buttonsToRemove = document.querySelectorAll('.rater button');
+        buttonsToRemove.forEach(button => {
+            button.remove();
+        });
+    }
+}
